e7dc7f78fbe3b1a865b0eedd6e8e6f0df8b3f754
pansan98 <edo.fjsjm@icloud.com>
2019-02-22 23:43:48 +0900

    設定ファイルの拡張

--- setting/BaseSetting.php
+++ setting/BaseSetting.php
@@ -9,6 +9,10 @@ class BaseSetting {
     private $_uri;
     private $_mainDir = 'games';
 
+    // bootstrapで使うフォルダの設定
+    protected $_autoLoadDir = [];
+
+    // 例外ファイル設定
     protected $_exceptionFile = [];
 
     public $_view;
@@ -30,6 +34,8 @@ class BaseSetting {
             define('LOCATION_URI', $uri);
         }
 
+        define('LOCATION_DIR', dirname(__FILE__));
+
         $this->setFilePathSetting();
     }
 
@@ -40,56 +46,53 @@ class BaseSetting {
         }
     }
 
+    /*
+     * ビューフォルダをセット
+     */
     protected function setViewFile($view)
     {
         $view = preg_replace(' ', '', $view);
         $this->_view = $view;
     }
 
+    /*
+     * ビューフォルダを取得
+     */
     protected function getViewFile()
     {
-        return LOCATION_DOMAIN.'/'.$this->_mainDir.'/'.$this->_fileName.'/'.$this->_view;
+        return LOCATION_DOMAIN.'/'.$this->_mainDir.'/'.$this->_gameName.'/'.$this->_view;
     }
 
+    /*
+     * 動作させるゲームフォルダをセット
+     */
     protected function setGames($gameName = 'sample')
     {
         $this->_gameName = $gameName;
     }
 
+    /*
+     * 動作させるゲームフォルダを取得
+     */
     protected function getGames()
     {
         return $this->_gameName;
     }
 
     /*
-     * テーマJSファイルをすべて読み込む（例外ファイルは別途設定）
+     * JSテーマファイルをすべて読み込む（例外ファイルは別途設定）
      * $jsDir String
-     * return String
      */
     public function getJsFile($jsDir)
     {
         if(is_dir($jsDir)) {
             // 除外ファイルが設定されていば処理終了
-            foreach ($this->getExceptionsFile() as $key => $val) {
-                if (strpos($key, $jsDir) !== false) {
-                    return false;
-                }
-            }
-            if ($openDir = opendir($jsDir)) {
-                foreach ($openDir as $file) {
-                    if (is_dir($file)) {
-                        $this->getJsFile($file);
-                    } else {
-                        if(file_exists(LOCATION_JS_PATH.$file)) {
-                            echo '<script type="text/javascript" src="'.LOCATION_JS_PATH.$file.'"></script>';
-                        }
-                    }
-                }
-            }
-        } else {
-            // 初期がファイルだったら読み込み
-            if(file_exists($jsDir)) {
-                echo '<script type="text/javascript" src="'.LOCATION_JS_PATH.$jsDir.'"></script>';
+            $exceptionFile = $this->checkExistsExceptionFile($jsDir);
+            if (count($exceptionFile) > 0) {
+                // TO DO 例外ファイルがあった時の処理
+                return false;
+            } else {
+                $this->getFiles($jsDir);
             }
         }
     }
@@ -105,12 +108,63 @@ class BaseSetting {
 
     /*
      * 例外ファイルをキーで取得
-     * return array
+     * $key String
      * $name String
+     * return String
      */
-    protected function getExceptionsFile($name = '')
+    protected function getExceptionsFiles($key, $name)
     {
-        return $this->_exceptionFile[$name];
+        foreach ($this->_exceptionFile[$key] as $keyFile => $valFile) {
+            if ($valFile == $name) {
+                return $valFile;
+            }
+        }
     }
+
+
+    /*
+     * キーで格納された配列を取得
+     * return array
+     * $key String
+     */
+    protected function checkExistsExceptionFile($key)
+    {
+        return $this->_exceptionFile[$key];
+    }
+
+    /*
+     * ファイルをすべて取得
+     * $dir String
+     * $isGet int
+     * param 0=>出力 1=>取得する
+     */
+    protected function getFiles($dir, $isGet = 0)
+    {
+        if (is_dir($dir)) {
+            if ($openDir = opendir($dir)) {
+                foreach ($openDir as $file) {
+                    if (is_dir($file)) {
+                        $this->getFiles($dir . $file);
+                    } else {
+                        if(file_exists(LOCATION_JS_PATH.$file)) {
+                            if ($isGet === 0) {
+                                echo '<script type="text/javascript" src="' . LOCATION_JS_PATH . $file . '"></script>';
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            // 初期がファイルだったら読み込み
+            if (is_file($dir)) {
+                if(file_exists(LOCATION_JS_PATH.$dir)) {
+                    if ($isGet === 0) {
+                        echo '<script type="text/javascript" src="'.LOCATION_JS_PATH.$dir.'"></script>';
+                    }
+                }
+            }
+        }
+    }
+
 }
 ?>
--- shooting/setting/ChildSetting.php
+++ shooting/setting/ChildSetting.php
@@ -7,11 +7,27 @@ class ChildSetting extends BaseSetting {
     // ゲームフォルダを指定
     protected $_gameFile = 'shooting';
 
+    //例外ファイルの設定
+    protected $_exFile = [];
+
     public function __construct()
     {
         parent::__construct();
         $this->setGameFile($this->_gameFile);
         $this->setViewFile($this->_gameFile);
+        $this->iniSetting();
+        //例外ファイルがあれば適用
+        //$this->setExceptionsFile('exception', 'sample.js');
+    }
+
+    private function iniSetting()
+    {
+        $this->_autoLoadDir[] = 'model/';
+    }
+
+    protected function createExceptionFiles($file)
+    {
+        $this->_exFile[] = $file;
     }
 
     private function setGameFile($name)
@@ -21,7 +37,7 @@ class ChildSetting extends BaseSetting {
 
     public function getGames()
     {
-        return parent::getGames();
+        return $this->getGames();
     }
 
     public function setViewFile($name)
@@ -32,6 +48,27 @@ class ChildSetting extends BaseSetting {
     public function getViewFile()
     {
         return parent::getViewFile();
+    }
+
+    public function setExceptionFile($key, $file)
+    {
+        $this->createExceptionFiles($file);
+        parent::setExceptionsFile($key, $this->_exFile);
+    }
+
+    public function getExceptionsFiles($key = 'sample', $name = 'sample.js') {
+        parent::getExceptionsFiles($key, $name);
+    }
+
+    public function outPutExceptionsFile($key, $name)
+    {
+        foreach ($this->_exceptionFile[$key] as $keyFile => $valFile) {
+            if ($valFile == $name) {
+                if (file_exists(LOCATION_JS_PATH.$key.'/'.$valFile)) {
+                    echo '<script type="text/javascript" src="'.LOCATION_JS_PATH.$key.'/'.$valFile.'"></script>';
+                }
+            }
+        }
     }
 }
 ?>
